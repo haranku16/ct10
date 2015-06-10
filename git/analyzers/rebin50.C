fe257928cd9b193f7a147309c3af19956e7c5a1a
Harankumar <haranku16@gmail.com>
2015-06-10 14:26:50 -0500

    Remade diphox_completed.root

---
+++ git/analyzers/rebin50.C
@@ -0,0 +1,21 @@
+#include <TH1.h>
+#include <TFile.h>
+void rebin50(){
+	TFile fin("diphox_1GeV.root");
+	TFile fout("diphox_completed.root","recreate");
+	double xbin[7] = {0,640,1130,1810,2610,3510,8600};
+	TH1D* h;
+	ostringstream stream; TString str;
+	for (int i =100; i < 153; i++){
+		stream.str(""); stream<<"BB"<<i; str = stream.str();
+		h = (TH1D*)fin.Get(str.Data());
+		h = (TH1D*)h->Clone();
+		h=(TH1D*)h->Rebin(5);
+		fout.cd(); h->Write();
+		stream.str(""); stream<<"BE"<<i; str = stream.str();
+		h = (TH1D*)fin.Get(str.Data());
+		h = (TH1D*)h->Clone();
+		h=(TH1D*)h->Rebin(5);
+		fout.cd(); h->Write();
+	}
+}
